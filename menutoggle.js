0eea74814554556ada0c73551286137aa830685b
CEFT15 <ceft15@CEFT15.local>
2023-10-10 18:41:54 -0400

    pushing work done on 10102023

---
+++ menutoggle.js
@@ -0,0 +1,30 @@
+// link trees
+// categories
+function categoriesToggle() {
+  var x = document.getElementById("categories");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+// footer menu post _tomorrow started_
+function postToggle() {
+    var x = document.getElementById("post");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+//footer menu archives  
+  function archivesToggle() {
+    var x = document.getElementById("archives");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
