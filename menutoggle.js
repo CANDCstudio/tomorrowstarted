867390f9feacea4ee8e414fee0607a237f180969
CEFT15 <ceft15@CEFT15.local>
2023-10-18 11:19:36 -0400

    work done on OCT/17/2023

--- menutoggle.js
+++ menutoggle.js
@@ -32,19 +32,28 @@ function referencesToggle() {
 // post _tomorrow started_
 function postToggle() {
     var x = document.getElementById("post");
-    if (x.style.display === "flex") {
+    if (x.style.display === "block") {
       x.style.display = "none";
     } else {
-      x.style.display = "flex";
+      x.style.display = "block";
     }
   }
 // archives  
   function archivesToggle() {
     var x = document.getElementById("archives");
-    if (x.style.display === "flex") {
+    if (x.style.display === "block") {
       x.style.display = "none";
     } else {
-      x.style.display = "flex";
+      x.style.display = "block";
+    }
+  }
+
+  function categoriesToggle() {
+    var x = document.getElementById("categories");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
     }
   }
   
